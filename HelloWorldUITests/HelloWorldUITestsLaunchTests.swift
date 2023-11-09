0c86d04b327385ba0e3d7945a036f6dc02332150
Matt Vollmer <matthewjvollmer@outlook.com>
2023-11-09 17:43:37 -0500

    add broken test

--- HelloWorldUITests/HelloWorldUITestsLaunchTests.swift
+++ HelloWorldUITests/HelloWorldUITestsLaunchTests.swift
@@ -20,9 +20,8 @@ final class HelloWorldUITestsLaunchTests: XCTestCase {
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
+        
+        app.staticTexts["Hey there!"].tap()
 
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
