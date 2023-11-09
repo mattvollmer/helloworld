053f1f5cb741fec98e92208cb4c4c81cf6635a35
Matt Vollmer <matthewjvollmer@outlook.com>
2023-11-09 17:12:13 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Matthew Vollmer on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
